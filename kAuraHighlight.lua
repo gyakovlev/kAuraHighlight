a3cf25c38b08cc1f7165503835a30160655edd63
affli <delycid@gmail.com>
2010-10-20 00:43:26 +0400

    fixes by Cadayron.

--- kAuraHighlight.lua
+++ kAuraHighlight.lua
@@ -1,18 +1,19 @@
--- defines
+ -- defines
 local AURAS = {
 		-- Death Knight
-		{ id = 48707 }, -- anti-magic shell
+		{ id = 53766 }, -- anti-magic shell
 		{ id = 48792 }, -- icebound fortitude
 		{ id = 49222 }, -- bone shield
 		{ id = 49039 }, -- lichborne
 		{ id = 48265 }, -- unholy presence
-		{ id = 48266 }, -- blood presence
-		{ id = 48263 }, -- frost presence
+		{ id = 48263 }, -- blood presence
+		{ id = 48266 }, -- frost presence
 		-- Druid
 		{ id = 8936 }, -- regrowth
 		{ id = 774 }, -- rejuvenation
 		{ id = 33763 }, -- lifebloom
 		{ id = 22812 }, -- barkskin
+		{ id = 48438 }, -- wild growth
 		-- Hunter
 		-- Mage
 		-- Priest
@@ -22,21 +23,21 @@ local AURAS = {
 		{ id = 6788 }, -- weakened soul
 		{ id = 33076, caster = "player" }, -- prayer of mending
 		{ id = 6346 }, -- fear ward
-		{ id = 552 }, -- abolish disease
 		-- Paladin
 		{ id = 6940 }, -- hand of sacrifice
-		{ forceId = 53601 }, -- sacred shield
+		{ id = 82327 }, -- Holy Radiance
 		{ id = 25771 }, -- forbearance
 		-- Rogue
-		{ id = 26669 }, -- evasion
+		{ id = 5277 }, -- evasion
 		-- Shaman
-		{ id = 49284 }, -- earth shield
+		{ id = 974 }, -- earth shield
+		{ id = 61295 }, -- Riptide
 		-- Warlock
 		-- Warrior
 	};
 
 local FRAME_MARGIN = 3;
-local ICON_MARGIN = 2;
+local ICON_MARGIN = 0;
 local ICON_SIZE = 22;
 	
 -- imports
@@ -62,8 +63,10 @@ end
 -- event handlers
 
 -- main
-HealthBarAuraFrame( oUF_Tukz_player, "TOP", 0, -ICON_MARGIN );
-HealthBarAuraFrame( oUF_Tukz_target, "TOP", 0, -ICON_MARGIN );
+	--HealthBarAuraFrame( oUF_Tukz_player, "TOP", 0, -ICON_MARGIN ); -- ORIGINAL VALUE
+	--HealthBarAuraFrame( oUF_Tukz_target, "TOP", 0, -ICON_MARGIN ); -- ORIGINAL VALUE	
+HealthBarAuraFrame( oUF_Tukz_player, "LEFT", 2, ICON_MARGIN );
+HealthBarAuraFrame( oUF_Tukz_target, "LEFT", 2, ICON_MARGIN );
 	
 for index = 1, 5 do
 	local frame = _G[ "oUF_Arena" .. tostring( index ) ]
@@ -89,7 +92,7 @@ do
 				break;
 			end
 			
-			HealthBarAuraFrame( frame, "TOPRIGHT", -ICON_MARGIN, -ICON_MARGIN );
+			HealthBarAuraFrame( frame, "CENTER", 0, 0 ); --HealthBarAuraFrame( frame, "TOPRIGHT", -ICON_MARGIN, -ICON_MARGIN ); ORIGINAL VALUE
 			partyFramesCreated = partyFramesCreated + 1
 		end
 		
